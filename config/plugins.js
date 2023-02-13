0b2dace573903c33e5512c599930c3d789919277
guritaalexandru <a_gurita@yahoo.com>
2023-02-13 16:20:25 +0200

    Added file7

---
+++ config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('IAM_USER_ACCESS_KEY'),
+        secretAccessKey: env('IAM_USER_SECRET_KEY'),
+        region: env('S3_BUCKET_REGION'),
+        params: {
+          Bucket: env('S3_BUCKET_NAME'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
